50b71e181b1a0663bbb3815ddabcb95fbdf2500b
Abdulfattah Zhgit config --global user.name 'Abdulfattah Zh'git config --global user.name Abdulfattah <Abdulfattah.zhilkibaev@gmail.com>
2021-05-31 13:22:14 +0600

    Complete Lesson 2 - First Task

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,18 @@
 let money = 100,
-    income,
-    addExpenses,
-    deposit,
-    mission,
-    period;
-alert('FIRST LOL YOU');
-console.log("SECOND LOL YOU ");
+    income = 30,
+    addExpenses = 'Taxi, Caffee, Burger, Internet',
+    deposit = false,
+    mission = 999,
+    period = 3,
+    budgetDay = 11.1;
+
+console.log(` Money:   type :  ${typeof (money)}   |  value :   ${money}`);
+console.log(` Income:   type :  ${typeof (income)}   |  value :   ${income}`);
+console.log(` Deposit:   type :  ${typeof (deposit)}   |  value :  ${deposit}`);
+
+console.log("Length: " + addExpenses.length + " symbols ");
+
+console.log(` Период равен : ${period} месяцев и Цель заработать ${mission} $`);
+
+console.log((addExpenses.toLocaleLowerCase()).split(', '));
+console.log('BudgetDay:' + budgetDay);
