16c4efd0d8f9fca8dac85b3f51c4edd2d63dec98
Abdulfattah Zh 🍩 <Abdulfattah.zhilkibaev@gmail.com>
2021-05-31 14:22:49 +0600

    Lesson_2 Hard Task complete check

--- scripts/hard.js
+++ scripts/hard.js
@@ -2,7 +2,7 @@ let num = 266219,
     numArray,
     sum = 1;
 numArray = String(num);
-numArray = numArray.match(/.{1}/g);
+numArray = numArray.match(/.{1}/g); // -- хотел бы лучше узнать об этом
 console.log(numArray);
 for (let i = 0; i < numArray.length; i++) {
     sum = sum * numArray[i];
@@ -13,4 +13,5 @@ sum = (sum * sum) * sum;
 numArray = String(sum);
 // numArray = numArray.match(/.{1}/g);
 console.log("Число :  " + numArray);
+
 console.log("Число :  " + numArray.substr(0, 2));
