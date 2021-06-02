c3b60e1beb71dff5b2712c9d1880c08b0ecfeab5
Abdulfattah Zh 🍩 <abdulfattah.zhilkibaev@gmail.com>
2021-06-02 16:26:29 +0600

    Update task 3

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,29 @@
-let money = 1,
-    money1 = 1,
-    getOut = 30,
-    getOut1 = 30,
-    addExpenses = 'Taxi, Caffee, Burger, Internet',
-    addExpenses1 = 'Taxi, Caffee, Burger, Internet',
-    deposit = false,
+let deposit = false,
     deposit1 = false,
     mission = 75000,
     mission1 = 75000,
     period = 0,
-    period1 = 0,
-    budgetDay = 1,
-    budgetDay1 = 1;
+    period1 = 0;
 
-console.log(` Money:   type :  ${typeof (money)}   |  value :   ${money}`);
-console.log(` Income:   type :  ${typeof (getOut)}   |  value :   ${getOut}`);
-console.log(` Deposit:   type :  ${typeof (deposit)}   |  value :  ${deposit}`);
+// console.log(` Money:   type :  ${typeof (money)}   |  value :   ${money}`);
+// console.log(` Income:   type :  ${typeof (getOut)}   |  value :   ${getOut}`);
+// console.log(` Deposit:   type :  ${typeof (deposit)}   |  value :  ${deposit}`);
 
-console.log("Length: " + addExpenses.length + " symbols ");
+// console.log("Length: " + addExpenses.length + " symbols ");
 
-console.log(` Период равен : ${period} месяцев и Цель заработать ${mission} $`);
+// console.log(` Период равен : ${period} месяцев и Цель заработать ${mission} $`);
 
-console.log((addExpenses.toLocaleLowerCase()).split(', '));
-console.log('BudgetDay:' + budgetDay);
-
-money = parseInt(prompt('Ваш месячный доход?'));
+// console.log((addExpenses.toLocaleLowerCase()).split(', '));
+// console.log('BudgetDay:' + budgetDay);
 
 
-addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую?');
-getOut = prompt('Укажите месячный расход: ');
-budgetDay = money - getOut;
+let money = parseInt(prompt('Ваш месячный доход?'));
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую?');
+let getOut = prompt('Укажите месячный расход: ');
+let budgetDay = (money - getOut) / 30;
 Math.floor(budgetDay);
 console.log("Чистая прибыль в день с учетом всех расходов : " + budgetDay); // dnevnoi dohod
-console.log("Время достижения цели : " + (Math.ceil(period = mission / (budgetDay * 30)))); // mission complete
+console.log("Время достижения цели : " + (Math.ceil(period = mission / budgetDay))); // mission complete
 
 
 if (budgetDay < 0)
@@ -47,13 +38,13 @@ else
 deposit = confirm('Есть ли у вас депозит в банке?');
 
 
-money1 = parseInt(prompt('Ваш месячный доход?'));
-addExpenses1 = prompt('Перечислите возможные расходы за рассчитываемый период через запятую?');
-getOut1 = prompt('Укажите месячный расход: ');
-budgetDay1 = money1 - getOut1;
-Math.floor(budgetDay);
+let money1 = parseInt(prompt('Ваш месячный доход?'));
+let addExpenses1 = prompt('Перечислите возможные расходы за рассчитываемый период через запятую?');
+let getOut1 = prompt('Укажите месячный расход: ');
+let budgetDay1 = (money1 - getOut1) / 30;
+Math.floor(budgetDay1);
 console.log("Чистая прибыль в день с учетом всех расходов : " + budgetDay1); // dnevnoi dohod
-console.log("Время достижения цели : " + (Math.ceil(period1 = mission1 / (budgetDay1 * 30)))); // mission complete
+console.log("Время достижения цели : " + (Math.ceil(period1 = mission1 / budgetDay1))); // mission complete
 
 if (budgetDay1 < 0)
     console.log("Что то пошло не так");
