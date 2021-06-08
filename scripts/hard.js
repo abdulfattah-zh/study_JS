fb0f98f1d411d9d9361cadb4ec567cb3e5e61c4e
abdulfattah-zh <80061047+abdulfattah-zh@users.noreply.github.com>
2021-06-08 09:33:35 +0600

    update r=hard

--- scripts/hard.js
+++ scripts/hard.js
@@ -18,14 +18,43 @@
 
 
 
-const checkString = function (itemForCheck) {
-    if (typeof itemForCheck != 'string') {
+// const checkString = function (itemForCheck) {
+//     if (typeof itemForCheck != 'string') {
 
-        return "PLS CHECK AGAIN, NOT A STRING!";
-    } else {
-        return itemForCheck.substr(0, 30) + "...";
+//         return "PLS CHECK AGAIN, NOT A STRING!";
+//     } else {
+//         return itemForCheck.substr(0, 30) + "...";
+//     }
+
+// };
+// console.log(checkString(121));
+// console.log(checkString("123456789123456789123456789000ERROR"));
+
+
+// 1) Создать массив arr = []
+// — Записать в него 7 любых многозначных чисел в виде строк
+// — Вывести в консоль только те, что начинаются с цифры 2 или 4(Должны присутствовать в массиве)
+//'1', '2', '2', '3', '4', '5'
+let arr = ['0'];
+
+for (let i = 0; i <= 6; i++) {
+    arr[i] = String(prompt("Запиши число: "));
+    //parseInt([i]);
+    console.log([i]);
+
+}
+console.log('------------------------');
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i].substr(0) == '2' || arr[i].substr(0) == '4') {
+        console.log("THESAME:::" + arr[i].substr(0));
     }
 
-};
-console.log(checkString(121));
-console.log(checkString("123456789123456789123456789000ERROR"));
+}
+
+console.log(arr);
+console.log('------------prostie------------');
+for (let i = 0; i < 100; i++) {
+    if (i % 2 == '1')
+        console.log("4islo:  " + i + ` Делители данного числа : 1 и ${i}`);
+}
