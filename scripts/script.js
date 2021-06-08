3fd4e2639a7d527403c34b50a2fcedc653309723
abdulfattah-zh <80061047+abdulfattah-zh@users.noreply.github.com>
2021-06-08 20:11:35 +0600

    fix while to for
    XD

--- scripts/script.js
+++ scripts/script.js
@@ -29,17 +29,12 @@ start();
 let expenses = [];
 let getExpensesMonth = function () {
   let sum = 0;
-  let i = 0;
-  do {
-    expenses[i] = prompt("Введите обязательную статью расходов?", "sadik1");
-    while (!isNumber(sum)) {
+  fot(let i = 0; i < 2; i++){
+      expenses[i] = prompt("Введите обязательную статью расходов?", "sadik1");
       sum += +prompt("Во сколько это обойдется?");
     }
-
     console.log(expenses);
-    i++;
     return sum;
-  } while (i < 2);
 };
 
 let expensesAmount = getExpensesMonth();
