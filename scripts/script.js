e8a83976090414845e10280de9ca63c28b0f5308
Abdulfattah Zh 🍩 <abdulfattah.zhilkibaev@gmail.com>
2021-06-09 17:14:38 +0600

    complete task 7

--- scripts/script.js
+++ scripts/script.js
@@ -1,39 +1,101 @@
 "use strict";
+
 let isNumber = function (n) {
-    return !isNaN(parseFloat(n) && isFinite(n));
+  return !isNaN(parseFloat(n) && isFinite(n));
 };
 
-function getRandom(min, max) {
-    return Math.random() * (max - min) + min;
+let money;
+let start = function () {
+  while (!isNumber(money)) {
+    money = +prompt("Ваш месячный доход?", 20000);
+  }
+};
+
+let appData = {
+  budget: money,
+  budgetDay: 0,
+  budgetMonth: 0,
+  income: {},
+  addIncome: [],
+  expenses: {},
+  addExpenses: [],
+  deposit: false,
+  mission: 50000,
+  period: 3,
+  asking: function () {
+    let addExpenses = prompt(
+      "Перечислите возможные расходы за рассчитываемый период через запятую?", "Ьензин, Садик, Вафли"
+    );
+    appData.addExpenses = addExpenses.toLowerCase().split(",");
+    appData.deposit = confirm("Есть ли у вас депозит в банке?");
+
+    //from GEM to asking
+    //let sum = 0;
+
+    for (let i = 0; i < 2; i++) {
+      appData.expenses[prompt("Введите обязательную статью расходов?", `sadik${i+1}`)] = +prompt("Во сколько это обойдется?", +(i + 1) * 1000);
+    }
+    console.log(appData.expenses);
+    //return sum;
+    //GEM 
+
+  },
+  getExpensesMonth: function () {
+    let sum = 0;
+    for (let key in appData.expenses) {
+      sum += appData.expenses[key];
+    }
+    console.log(sum);
+    return sum;
+  },
+  getBudget: function () {
+    // let buffer = appData.getExpensesMonth();
+    let amoutCount = appData.getExpensesMonth();
+    appData.budgetMonth = money - amoutCount;
+    appData.budgetDay = Math.floor(appData.budgetMonth / 30);
+    return appData.budgetDay;
+    // return money - buffer;
+  },
+  getTargetMonth: function () {
+    // let buffer = appData.getExpensesMonth();
+    let target = Math.ceil((appData.period = appData.mission / appData.budgetMonth));
+    console.log(target);
+    if (target <= 0) {
+      return "Цель не будет достигнута ";
+    } else {
+      return "Цель будет достигнута ";
+    }
+    // mission complete
+  },
+
+  //
+  getStatusIncome: function () {
+    if (appData.budgetDay < 0) return "Что то пошло не так";
+    else if (appData.budgetDay >= 1200) return "У вас высокий уровень дохода";
+    else if (appData.budgetDay >= 600 || appData.budgetDay <= 1199)
+      return "У вас средний уровень дохода";
+    else return "К сожалению у вас уровень дохода ниже среднего";
+  }
+  //
+};
+start();
+appData.asking();
+console.log("Расходы за месяц : " + appData.getExpensesMonth());
+console.log(
+  "Время достижения цели : " + appData.period + " месяца"
+);
+
+console.log("Чистая прибыль в день с учетом всех расходов : " + appData.budgetDay); // dnevnoi dohod
+
+console.log(appData.getStatusIncome(appData.deposit)); //
+console.log("            ");
+console.log("            ");
+console.log("            ");
+console.log("-----------------Наша программа включает в себя----------------------");
+for (let key in appData) {
+  console.log("            ");
+  console.log("Ключ:  " + key + "   Значение: " + appData[key]);
+  console.log("            ");
 }
 
-let gameBot = function () {
-    let gameNumber = Math.ceil(getRandom(1, 100));
-    console.log(gameNumber);
-    let gameStart = function (gameNumber) {
-        let userNumber = prompt("Введите число от 1  до 100");
-
-        console.log("type of : -" + typeof userNumber);
-
-        if (isNumber(userNumber)) {
-            //--
-            if (userNumber > gameNumber) {
-                alert("Загаданное число меньше");
-                gameStart(gameNumber);
-            } else if (userNumber < gameNumber) {
-                alert("Загаданное число больше");
-                gameStart(gameNumber);
-            } else {
-                alert("Загаданное число : " + gameNumber + " WIN!!!");
-            }
-            //--
-        } else if (typeof userNumber === "string") {
-            alert("Введите число!");
-            gameStart(gameNumber);
-        } else {
-            return alert("Game Canceled");
-        }
-    };
-    return gameStart(gameNumber);
-};
-gameBot();
+//==========================================7lesson==========================
