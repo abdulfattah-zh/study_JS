dea80b95d16431053529e90478bdaad9f3281065
abdulfattah-zh <80061047+abdulfattah-zh@users.noreply.github.com>
2021-06-10 05:15:44 +0600

    Change check

--- scripts/hard.js
+++ scripts/hard.js
@@ -1,13 +1,13 @@
 "use strict";
 
-let nowDay = 3;
+let nowDay = 'Среда';
 let weekArray = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
 let weekAlert = '';
 for (let i = 0; i < weekArray.length; i++) {
 
-    if (i === nowDay) {
+    if (weekArray[i] === nowDay) {
         weekAlert += "\nДень : " + weekArray[i].bold();
-    } else if (i === 5 || i === 6) {
+    } else if (weekArray[i] === 'Суббота' || weekArray[i] === 'Воскресенье') {
         weekAlert += "\nДень : " + weekArray[i].italics();
     } else {
         weekAlert += "\nДень : " + weekArray[i];
