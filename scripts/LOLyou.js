e875072e9ce16da3b95aa7b4c569566239c72928
AF <af@gmail.com>
2021-06-22 20:20:01 +0600

    lesson 14 complete

--- scripts/LOLyou.js
+++ scripts/LOLyou.js
@@ -8,18 +8,26 @@ const DomElement = function () {
     this.fontSize;
 };
 
-DomElement.prototype.addElement = function (selector) {
+DomElement.prototype.addElement = function (selector, _height, _width, _bg, _fontSize) {
 
     this.selector = selector;
     console.log(this.selector);
     if (selector.substr(0, 1) == '.') {
         let newDiv = document.createElement('div');
+        newDiv.style.height = _height + 'px';
+        newDiv.style.width = _width + 'px';
+        newDiv.style.backgroundColor = _bg;
+        newDiv.style.fontSize = _fontSize + 'px';
         newDiv.innerHTML = "1111111111111111111111111111111111111111111111111111111111111111111111111111111111";
         newDiv.classList.add(selector.substr(1, ));
         document.body.appendChild(newDiv);
     } else {
         let newDiv = document.createElement('p');
-        newDiv.innerHTML = "2222222222222222222222222222222222222222222222222222222222222222222222222222222222";
+        newDiv.style.height = _height + 'px';
+        newDiv.style.width = _width + 'px';
+        newDiv.style.backgroundColor = _bg;
+        newDiv.style.fontSize = _fontSize + 'px';
+        newDiv.textContent = "2222222222222222222222222222222222222222222222222222222222222222222222222222222222";
         newDiv.id = (selector.substr(1, ));
         document.body.appendChild(newDiv);
     }
@@ -28,5 +36,5 @@ DomElement.prototype.addElement = function (selector) {
 
 const domElement = new DomElement();
 
-domElement.addElement('.div');
+domElement.addElement('.div', );
 domElement.addElement('#div');
