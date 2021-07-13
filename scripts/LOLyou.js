a2b1f6c516888267ce933814b279f89c3ce630e0
AF <af@gmail.com>
2021-07-13 15:44:31 +0600

    complete task 17

--- scripts/LOLyou.js
+++ scripts/LOLyou.js
@@ -1,15 +1,94 @@
 "use strict";
+let myLesson = [{
+        lesson: 1,
+        type: 'basic',
+        points: 2
+    },
+    {
+        lesson: 2,
+        type: 'additional',
+        points: 4
+    },
+    {
+        lesson: 3,
+        type: 'basic',
+        points: 6
+    },
+    {
+        lesson: 4,
+        type: 'additional',
+        points: 3
+    },
+    {
+        lesson: 5,
+        type: 'basic',
+        points: 4
+    },
+    {
+        lesson: 6,
+        type: 'basic',
+        points: 2
+    },
+    {
+        lesson: 7,
+        type: 'additional',
+        points: 2
+    },
+    {
+        lesson: 8,
+        type: 'basic',
+        points: 6
+    },
+    {
+        lesson: 9,
+        type: 'basic',
+        points: 4
+    },
+    {
+        lesson: 10,
+        type: 'basic',
+        points: 6
+    },
+    {
+        lesson: 11,
+        type: 'additional',
+        points: 5
+    },
+    {
+        lesson: 12,
+        type: 'basic',
+        points: 2
+    },
+    {
+        lesson: 13,
+        type: 'additional',
+        points: 2
+    },
+    {
+        lesson: 14,
+        type: 'basic',
+        points: 4
+    },
+    {
+        lesson: 15,
+        type: 'additional',
+        points: 1
+    },
+    {
+        lesson: 16,
+        type: 'additional',
+        points: 7
+    },
+];
 
-class First {
-    hello() {
-        console.log("Привет Я Метод родителя by First");
+myLesson = myLesson.filter((el) => {
+    return el.type !== 'additional'
+}).map((item) => {
+
+    return {
+        ...item,
+        points: item.points / 2
     }
-}
-class Second extends First {
-    hello() {
-        super.hello();
-        console.log("Привет А я наследуемый метод by Second ");
-    }
-}
-let firstSecond = new Second();
-console.log(firstSecond.hello());
+})
+
+console.log(myLesson)
